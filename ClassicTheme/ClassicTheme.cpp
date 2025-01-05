40079a5a4fe284d1d58e37a3fb9f12359e7caa71
Ingan121 <lssaa91192@gmail.com>
2025-01-05 14:13:16 +0900

    1.2

--- ClassicTheme/ClassicTheme.cpp
+++ ClassicTheme/ClassicTheme.cpp
@@ -1,6 +1,8 @@
 ﻿// ClassicTheme.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
 #include <Windows.h>
 #include <iostream>
 #include <sddl.h>
